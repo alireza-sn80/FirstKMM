ed482e247db3425e6c605c860f1a06c60b7ebf9b
Alirezasno80 <96535925+alireza-sn80@users.noreply.github.com>
2022-10-14 16:38:28 +0330

    setup androidApp di ktor

--- androidApp/src/main/java/com/alireza/firstkmmapp/android/di/NetworkModule.kt
+++ androidApp/src/main/java/com/alireza/firstkmmapp/android/di/NetworkModule.kt
@@ -1,10 +1,14 @@
 package com.alireza.firstkmmapp.android.di
 
+import com.alireza.firstkmmapp.data.network.ApiService
+import com.alireza.firstkmmapp.data.network.ApiServiceImp
+import com.alireza.firstkmmapp.data.network.KtorClientFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.*
+import io.ktor.http.Headers.Companion.build
 import javax.inject.Singleton
 
 @Module
@@ -14,9 +18,8 @@ object NetworkModule {
     @Singleton
     @Provides
     fun provideHttpClient(): HttpClient {
-        return KtorClientFactory.build()
+        return KtorClientFactory().build()
     }
-
 
     @Singleton
     @Provides
@@ -27,6 +30,5 @@ object NetworkModule {
             token = "2304923jedmw092d0293ke2ed"
         )
     }
-
 
 }
